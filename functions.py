55b523ba7be5ebc7af4952773c805193772ce07c
Debora <deboramoreiracastro@gmail.com>
2023-11-22 21:26:02 -0300

    Proteção do bd

--- functions.py
+++ functions.py
@@ -185,9 +185,9 @@ def delete_sql(connect):
 def consulta1(connect):
     select_query = """
     select g.nome, avg(m.duracao) as media
-    from Musica as m
-    join Genero_Musica as gm on m.id_midia = gm.id_midia
-    join Genero as g on gm.id_genero = g.id_genero
+    from musica as m
+    join genero_Musica as gm on m.id_midia = gm.id_midia
+    join genero as g on gm.id_genero = g.id_genero
     group by g.nome
     """
     print("Primeira Consulta: Selecione a média da duração das músicas por gênero musical.")
@@ -218,9 +218,9 @@ def consulta2(connect):
     select_query = """
     select u.nome as usuario, 
     count(*) as qtd_musicas_tocadas, sum(mi.duracao) as duracao_total_musicas_tocadas
-    from Usuario as u
-    join Reproducao as r on r.id_usuario = u_usuario
-    join Midia as mi on mi.id_midia = r.id_midiaa
+    from usuario as u
+    join reproducao as r on r.id_usuario = u_usuario
+    join midia as mi on mi.id_midia = r.id_midiaa
     group by u.nome
     """
     print("\nSegunda Consulta: Selecione a quantidade total de músicas tocadas e a duração total considerando a duração individual de cada música por usuario.")
@@ -262,10 +262,10 @@ def consulta2(connect):
 def consulta3(connect):
     select_query = """
     select u.nome, u.e-mail, avg(m.qtde_streamings) as media_streamings
-    from Assinatura as a
-    join Usuario as u on u.id_plano = a.id_plano
-    join Reproducao as r on r.id_usuario = u.id_usuario
-    join Midia as m on m.id_midia = r.id_midia
+    from assinatura as a
+    join usuario as u on u.id_plano = a.id_plano
+    join reproducao as r on r.id_usuario = u.id_usuario
+    join midia as m on m.id_midia = r.id_midia
     where r.data between '01/10/2023' and '31/10/2023'
     and a.nome = 'Gratuito'
     group by u.nome, u.e-mail
--- settings.py
+++ settings.py
@@ -1,10 +1,3 @@
-# LIB IMPORTS
-import psycopg2
-from psycopg2 import errorcodes
-from pydub import AudioSegment
-from pydub.playback import play
-import matplotlib.pyplot as plt
-
 # POSTGRESQL SETTINGS
 DB_NAME = 'postgres'
 DB_USER = 'postgres'
