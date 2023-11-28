8b22094814a2bb9c43ff4a3c8201fe48dfc892ff
miltonspacheco <miltonsilveirapacheco@gmail.com>
2023-11-27 23:17:05 -0300

    foi

--- sql_files/inserts.sql
+++ sql_files/inserts.sql
@@ -142,8 +142,8 @@ INSERT INTO Midia (id_midia, nome, duracao, qtd_streamings, data_lancamento, con
         (20, 'Loki 2: O glorioso propósito do deus da trapaça - NerdCast', '04:34', 0, '2023-11-17', 'media/podcast/nerdcast_908_loki_2.mp3'),
         (21, 'SPACE TODAY - Flow Podcast', '02:17:51', 0, '2020-10-21', 'media/podcast/SPACE_TODAY_FlowPodcast.mp3'),
         (22, 'ANDERSON SILVA - Flow Podcast', '03:37:43', 0, '2023-11-15', 'media/podcast/Anderson_Silva_FlowPodcast.mp3'),
-        (23, 'CLÓVIS DE BARROS FILHO - Flow Podcast', '02:17:51', 0, '2023-10-10', 'media/podcast/CLOVIS_DE_BARROS_FILHO_FlowPodcast.mp3'),
-        (24, 'TAÍS ARAUJO - Podpah', '01:56:16', 0, '2023-03-20', 'media/podcast/TAIS_ARAUJO_Podpah.mp3'),
+        (23, 'CLOVIS DE BARROS FILHO - Flow Podcast', '02:17:51', 0, '2023-10-10', 'media/podcast/CLOVIS_DE_BARROS_FILHO_FlowPodcast.mp3'),
+        (24, 'TAIS ARAUJO - Podpah', '01:56:16', 0, '2023-03-20', 'media/podcast/TAIS_ARAUJO_Podpah.mp3'),
         (25, 'ANDRIANO IMPERADOR - Podpah', '01:21:13', 0, '2023-07-14', 'media/podcast/Anderson_Silva_FlowPodcast.mp3');
 
 
